4315d66a1996ba5de6a82910a4c8cb35ba61ce04
Maxime Certain <MaximeCertain>
2020-05-04 16:59:54 +0200

    chat

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,35 @@
 const WebSocket = require('ws');
+//process.env.WS_PORT;
+
+const wss = new WebSocket.Server({
+    port: process.env.WS_PORT
+});
+
+/**
+ * webSocket => le client
+ */
+wss.on('connection', webSocket => {
+    console.log("conneciton ok");
+    webSocket.on('close', () => {
+        console.log("connection closed by the client");
+    });
+
+    webSocket.on('message', (msgEvent) => {
+        wss.clients.forEach(function each(client) {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(msgEvent);
+            }
+        })
+    });
+
+    webSocket.send(JSON.stringify({
+        user: "server",
+        text: "welcome ! "
+    }))
+    //  webSocket.send("New connexion");
+
+    //webSocket.emit('new_chatter', webSocket)
+});
+wss.on('error', () => {
+    console.log("errored");
+});
